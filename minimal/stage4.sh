932429f389010fbaf3de3af5ef37fcdfff82afc8
Marcus Behel <marcus.behel@mindspring.com>
2019-07-19 23:39:41 -0400

    Completed stage 4 script

---
+++ minimal/stage4.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+function print_status(){
+    if [ $? -eq 0 ]
+    then
+	printf "Done.\n"
+    else
+	printf "Failed.\n"
+	exit 1
+    fi
+}
+
+function print_if_fail(){
+    if [ $? -ne 0 ]
+    then
+	printf "Failed.\n"
+    fi
+}
+
+LOGFILE=/root/setup_log.txt
+
+################################################################################
+# Prechecks (running as root and has intenet access)
+################################################################################
+
+if ! [ $(id -u) = 0 ]; then
+   echo "Run this script as root!"
+   exit 1
+fi
+
+if ping -q -c 1 -W 1 google.com >/dev/null; then
+    true
+else
+    echo "Connect to the internet before running this script!"
+    exit 1
+fi
+
+last=$(cat /root/last_setup_stage.txt)
+if [ "$last" != "stage3" ]
+then
+    printf "Run stage 3 first.\n"
+fi
+
+################################################################################
+# Stage operations
+################################################################################
+
+printf "Installing python3 for ArPiRobot code..."
+apt-get -y install python3 python3-pip python3-setuptools python3-wheel >> $LOGFILE 2>&1
+print_status
+
+printf "Installing required python3 libraries..."
+pip3 install apscheduler ansicolors pyserial adafruit-circuitpython-motorkit >> $LOGFILE 2>&1
+print_status
+
+printf "Cloning ArPiRobot PythonLib..."
+git clone git@github.com:MB3hel/ArPiRobot-PythonLib.git /home/pi/ArPiRobot-PythonLib >> $LOGFILE 2>&1
+print_status
+
+printf "Installing ArPiRobot PythonLib..."
+cd /home/pi/ArPiRobot-PythonLib/ >> $LOGFILE 2>&1
+print_if_fail
+python3 setup.py install >> $LOGFILE 2>&1
+print_status
+
+printf "Making ArPiRobot directory..."
+su - pi -c "mkdir -p /home/pi/arpirobot/"  >> $LOGFILE 2>&1
+print_status
+
+printf "Setting up test program..."
+cp /home/pi/ArPiRobot-PythonLib/samples/test.py /home/pi/arpirobot/ >> $LOGFILE 2>&1
+print_if_fail
+su - pi -c 'echo "test.py" | tee /home/pi/arpirobot/main.txt' >> $LOGFILE 2>&1
+print_status
+
+printf "Clearing WiFi network settings..."
+printf "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\ncountry=US\n\nnetwork={\n        ssid="DUMMY_NETWORK"\n        psk="DUMMY_PASSWORD"\n        id_str="AP1"\n}" | tee /etc/wpa_supplicant/wpa_supplicant.conf >> $LOGFILE 2>&1
+print_status
+
+printf "Clearing bash history..."
+history -c >> $LOGFILE 2>&1
+print_if_fail
+su - pi -c "history -c" >> $LOGFILE 2>&1
+print_status
+
+printf "Removing ssh keys..."
+rm -rf /home/pi/.ssh/* >> $LOGFILE 2>&1
+print_if_fail
+rm -rf /root/.ssh/*  >> $LOGFILE 2>&1
+print_status
+
+
+################################################################################
+# Restart
+################################################################################
+
+printf "The minimal image configuration is now setup on this Pi. The system will now reboot.\nMake sure to test the image and make sure all sensitive information has been removed.\nPress enter to reboot now..."
+read n
+
+echo "stage4" > /root/last_setup_stage.txt
+
+reboot
