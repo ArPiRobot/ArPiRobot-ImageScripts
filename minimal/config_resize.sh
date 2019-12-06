828ff0ac876b06c3f33cd3bcc71e7247e39df707
Marcus Behel <marcus.behel@mindspring.com>
2019-12-06 12:31:42 -0500

    Escaped slashes in sed command

--- minimal/config_resize.sh
+++ minimal/config_resize.sh
@@ -8,7 +8,7 @@ if ! [ $(id -u) = 0 ]; then
 fi
 
 # Append quiet init=/usr/lib/raspi-config/init_resize.sh to the end of the first line in the file
-sed '1 s/$/ quiet init=/usr/lib/raspi-config/init_resize.sh/' /boot/cmdline.txt
+sed '1 s/$/ quiet init=\/usr\/lib\/raspi-config\/init_resize.sh/' /boot/cmdline.txt
 
 # Write script
 printf "#!/bin/sh\n### BEGIN INIT INFO\n# Provides:          resize2fs_once\n# Required-Start:\n# Required-Stop:\n# Default-Start: 3\n# Default-Stop:\n# Short-Description: Resize the root filesystem to fill partition\n# Description:\n### END INIT INFO\n. /lib/lsb/init-functions\ncase \"$1\" in\n  start)\n    log_daemon_msg \"Starting resize2fs_once\"\n    ROOT_DEV=$(findmnt / -o source -n) &&\n    resize2fs $ROOT_DEV &&\n    update-rc.d resize2fs_once remove &&\n    rm /etc/init.d/resize2fs_once &&\n    log_end_msg $?\n    ;;\n  *)\n    echo \"Usage: $0 start\" >&2\n    exit 3\n    ;;\nesac\n" > /etc/init.d/resize2fs_once
