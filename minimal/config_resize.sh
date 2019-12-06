ddac54508b4923848bc0967a7560c2e159fc0fd0
Marcus Behel <marcus.behel@mindspring.com>
2019-12-06 12:27:37 -0500

    Different script to do resize

---
+++ minimal/config_resize.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Note: Run as root
+
+if ! [ $(id -u) = 0 ]; then
+   echo "Run this script as root!"
+   exit 1
+fi
+
+# Append quiet init=/usr/lib/raspi-config/init_resize.sh to the end of the first line in the file
+sed '1 s/$/ quiet init=/usr/lib/raspi-config/init_resize.sh/' /boot/cmdline.txt
+
+# Write script
+printf "#!/bin/sh\n### BEGIN INIT INFO\n# Provides:          resize2fs_once\n# Required-Start:\n# Required-Stop:\n# Default-Start: 3\n# Default-Stop:\n# Short-Description: Resize the root filesystem to fill partition\n# Description:\n### END INIT INFO\n. /lib/lsb/init-functions\ncase \"$1\" in\n  start)\n    log_daemon_msg \"Starting resize2fs_once\"\n    ROOT_DEV=$(findmnt / -o source -n) &&\n    resize2fs $ROOT_DEV &&\n    update-rc.d resize2fs_once remove &&\n    rm /etc/init.d/resize2fs_once &&\n    log_end_msg $?\n    ;;\n  *)\n    echo \"Usage: $0 start\" >&2\n    exit 3\n    ;;\nesac\n" > /etc/init.d/resize2fs_once
+
+# Enable service next boot (it will delete itself after running
+chmod +x /etc/init.d/resize2fs_once
+systemctl enable resize2fs_once
+
+printf "The system will expand the root partition and filesystem on next boot. \nPress enter to power off..."
+read n
+
+poweroff
--- minimal/stage4.sh
+++ minimal/stage4.sh
@@ -111,23 +111,13 @@ printf "Clearing ssh host keys..."
 rm -f /etc/ssh/ssh_host_* >> $LOGFILE 2>&1
 print_status
 
-printf "Setting up to expand root fs at next boot..."
-echo "init=/usr/lib/raspi-config/init_resize.sh" | tee -a /boot/config.txt
-print_if_fail
-printf "#!/bin/sh\n### BEGIN INIT INFO\n# Provides:          resize2fs_once\n# Required-Start:\n# Required-Stop:\n# Default-Start: 3\n# Default-Stop:\n# Short-Description: Resize the root filesystem to fill partition\n# Description:\n### END INIT INFO\n. /lib/lsb/init-functions\ncase \"\$1\" in\n  start)\n    log_daemon_msg \"Starting resize2fs_once\" &&\n    resize2fs /dev/$ROOT_PART &&\n    update-rc.d resize2fs_once remove &&\n    rm /etc/init.d/resize2fs_once &&\n    log_end_msg \$?\n    ;;\n  *)\n    echo \"Usage: \$0 start\" >&2\n    exit 3\n    ;;\nesac\n" | tee /etc/init.d/resize2fs_once
-print_if_fail
-chmod +x /etc/init.d/resize2fs_once >> $LOGFILE 2>&1
-print_if_fail
-systemctl enable resize2fs_once
-print_status
-
 ################################################################################
 # Restart
 ################################################################################
 
 printf "\n\n-------------------------\nEND OF STAGE 4\n-------------------------\n\n" >> $LOGFILE 2>&1
 
-printf "The minimal image configuration is now setup on this Pi. The system will now reboot.\nMake sure to test the image and make sure all sensitive information has been removed.\nThe image will expand on next reboot.\nPress enter to power off now..."
+printf "The minimal image configuration is now setup on this Pi. The system will now reboot.\nAfter rebooting, make sure to test the image and make sure all sensitive information has been removed, then run the config_resize script to make the system expand on the next boot. Press enter to reboot..."
 read n
 
 echo "stage4" > /root/last_setup_stage.txt
