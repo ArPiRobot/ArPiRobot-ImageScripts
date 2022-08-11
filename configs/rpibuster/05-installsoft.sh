202cbdb75d93499c7a038912ddcba0329e7f4570
Marcus Behel <marcus.behel@mindspring.com>
2022-08-11 10:58:15 -0400

    Implement rpibuster installsoft

--- configs/rpibuster/05-installsoft.sh
+++ configs/rpibuster/05-installsoft.sh
@@ -11,3 +11,69 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap exit_trap EXIT
 
 
+# Get architecture of an ELF binary
+function binarch(){
+    printf "$(readelf -h $1 | grep Machine: | sed -r 's/\s+Machine:\s+//g')"
+}
+
+# Install required software from system repos
+apt-get -y install git \
+    openssh-server \
+    dos2unix \
+    sysstat \
+    python3 \
+    python3-pip \
+    python3-setuptools \
+    python3-setuptools-scm \
+    python3-wheel \
+    openjdk-8-jdk-headless \
+    libasound2-dev \
+    iperf3 \
+    libgstreamer1.0-0 \
+    gstreamer1.0-plugins-base \
+    gstreamer1.0-plugins-base-apps \
+    gstreamer1.0-plugins-good \
+    gstreamer1.0-plugins-bad \
+    gstreamer1.0-plugins-ugly \
+    gstreamer1.0-libav \
+    gstreamer1.0-tools \
+    gstreamer1.0-alsa \
+    gstreamer1.0-pulseaudio \
+    python3-gi \
+    python3-gst-1.0 \
+    gstreamer1.0-gl \
+    gstreamer1.0-rtsp \
+    gstreamer1.0-omx-rpi-config \
+    gstreamer1.0-omx-rpi \
+    gstreamer1.0-omx
+
+# Install ArPiRobot-CameraStreaming
+cd /home/arpirobot
+git clone https://github.com/ArPiRobot/ArPiRobot-CameraStreaming.git
+chown -R arpirobot:arpirobot ArPiRobot-CameraStreaming
+cd ArPiRobot-CameraStreaming
+chmod +x ./install.sh
+./install.sh arpirobot
+arch=$(binarch $(which python3))
+if [ "$arch" = "ARM" ]; then
+    chmod +x ./install_rtsp_server_armv6.sh
+    ./install_rtsp_server_armv6.sh arpirobot
+elif [ "$arch" = "AArch64" ]; then
+    chmod +x ./install_rtsp_server_aarch64.sh
+    ./install_rtsp_server_aarch64.sh arpirobot
+else
+    echo "Unknown architecture. Cannot install rtsp-simple-server."
+    exit 1
+fi
+sed -i 's/libcamera/raspicam/g' /home/arpirobot/camstream/default.txt
+
+# Install ArPiRobot-Tools
+cd /home/arpirobot
+git clone https://github.com/ArPiRobot/ArPiRobot-Tools.git
+cd ArPiRobot-Tools
+chmod +x ./install.sh
+./install.sh arpirobot
+
+# Create directory for robot program
+mkdir -p /home/arpirobot/arpirobot
+chown arpirobot:arpirobot /home/arpirobot/arpirobot
