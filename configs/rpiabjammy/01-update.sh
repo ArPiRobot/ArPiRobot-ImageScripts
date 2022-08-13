f24c4ca53023f8943e553d4cf5b47bdb21052e64
Marcus Behel <marcus.behel@mindspring.com>
2022-08-13 01:14:37 -0400

    Added note about flash-kernel...

--- configs/rpiabjammy/01-update.sh
+++ configs/rpiabjammy/01-update.sh
@@ -10,6 +10,8 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap exit_trap EXIT
 
+# This is going to be fun...
+# See https://github.com/armbian/build/blob/master/extensions/flash-kernel.sh
 
 apt-get -y update
 apt-get -y -o Dpkg::Options::=--force-confold -o Dpkg::Options::=--force-confdef upgrade
