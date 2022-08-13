82b6dab30bceffa6c71d0c41eb4dbec87408a34b
Marcus Behel <marcus.behel@mindspring.com>
2022-08-13 10:18:29 -0400

    Potential fix for flash-kernel and initramfs-tools when upgrading / installing packages rpiabjammy

--- configs/rpiabjammy/01-update.sh
+++ configs/rpiabjammy/01-update.sh
@@ -10,9 +10,20 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap exit_trap EXIT
 
-# This is going to be fun...
-# See https://github.com/armbian/build/blob/master/extensions/flash-kernel.sh
+# Disable flash-kernel hooks because they don't work right in chroot
+# See process used by https://github.com/armbian/build/blob/master/extensions/flash-kernel.sh
+chmod -x /etc/kernel/postinst.d/initramfs-tools
+chmod -x /etc/initramfs/post-update.d/flash-kernel
 
 apt-get -y update
 apt-get -y -o Dpkg::Options::=--force-confold -o Dpkg::Options::=--force-confdef upgrade
 dpkg --configure -a
+
+
+# Packages have now changed. Need to manually make update-initramfs and flash-kernel work
+update-initramfs -c -k all
+flash-kernel --machine 'Raspberry Pi 4 Model B'
+
+# Enable flash-kernel hooks again
+chmod +x /etc/kernel/postinst.d/initramfs-tools
+chmod +x /etc/initramfs/post-update.d/flash-kernel
