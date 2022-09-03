a4d326a6c6f0de9ca803358ac95f42a4af1b1c62
Marcus Behel <marcus.behel@mindspring.com>
2022-09-03 15:01:23 -0400

    Added (untested) scripts for opilite armbian jammy image

---
+++ configs/opiliteabjammy/02-users.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+function exit_trap(){
+    ec=$?
+    if [ $ec -ne 0 ]; then
+        echo "\"${last_command}\" command failed with exit code $ec."
+    fi
+}
+set -e
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap exit_trap EXIT
+
+# Add "arpirobot" user
+adduser --disabled-password --gecos "" arpirobot
+printf "arpirobot\narpirobot" | passwd arpirobot
+
+# Copy all groups of default user
+groups=("tty" "disk" "dialout" "sudo" "audio" "video" "plugdev" "games" "users" "systemd-journal" "input" "netdev")
+for i in "${groups[@]}" ; do
+    addgroup arpirobot $i
+done
+
+# Setting root password
+printf "notdefault\nnotdefault" | passwd root
+
+# Allow passwordless sudo for arpirobot
+echo "arpirobot ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/010_arpirobot-nopasswd
+
