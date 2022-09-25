1cb066198ac70f7057c58a56421cde40880f88b3
Marcus Behel <marcus.behel@mindspring.com>
2022-09-25 17:36:05 -0400

    Fix hostapd typo

--- configs/opii96bullseye/06-network.sh
+++ configs/opii96bullseye/06-network.sh
@@ -19,7 +19,7 @@ systemctl enable hostapd
 systemctl enable dnsmasq
 
 # Write hostpad config files
-mkdir -p /etc/hostpad
+mkdir -p /etc/hostapd
 cat > /etc/hostapd/hostapd.conf << 'EOF'
 country_code=US
 ieee80211d=1
