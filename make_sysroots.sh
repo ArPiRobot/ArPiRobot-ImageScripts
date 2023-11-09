d04a3fbefa4899a002a00dbff38695b138b8e025
Marcus Behel <marcus.behel@mindspring.com>
2023-11-09 00:13:37 -0500

    Switch to buildd variant not minbase

--- make_sysroots.sh
+++ make_sysroots.sh
@@ -26,11 +26,13 @@ cd $BUILDDIR
 
 # armv6 chroot is based on raspbian not debian b/c debian armhf is armv7 but Pi zero is armv6 w/ hard float
 rm -rf ./bootstrap-armv6
-debootstrap --arch=armhf --variant=minbase $codename ./bootstrap-armv6 http://raspbian.raspberrypi.org/raspbian/
+debootstrap --arch=armhf --variant=buildd $codename ./bootstrap-armv6 http://raspbian.raspberrypi.org/raspbian/
 rm -rf ./sysroot-armv6
 mkdir ./sysroot-armv6
 ../sysroot-from-root.sh ./bootstrap-armv6 ./sysroot-armv6
 cd ./sysroot-armv6
+rm -f ../sysroot-armv6.tar
+rm -f ../sysroot-armv6.tar.gz
 tar -cvf ../sysroot-armv6.tar *
 gzip ../sysroot-armv6.tar
 cd ..
@@ -38,11 +40,13 @@ rm -rf ./sysroot-armv6
 
 # aarch64 chroot is based on normal debian
 rm -rf ./bootstrap-aarch64
-sudo debootstrap --arch=arm64 --variant=minbase $codename ./bootstrap-aarch64
+sudo debootstrap --arch=arm64 --variant=buildd $codename ./bootstrap-aarch64
 rm -rf ./sysroot-aarch64
 mkdir ./sysroot-aarch64
 ../sysroot-from-root.sh ./bootstrap-aarch64 ./sysroot-aarch64
 cd ./sysroot-aarch64
+rm -f ../sysroot-aarch64.tar
+rm -f ../sysroot-aarch64.tar.gz
 tar -cvf ../sysroot-aarch64.tar *
 gzip ../sysroot-aarch64.tar
 cd ..
