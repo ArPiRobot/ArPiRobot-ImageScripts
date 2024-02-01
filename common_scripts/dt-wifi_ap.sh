984251793b8d83ba447a157a455d7d43705fb421
Marcus Behel <marcus.behel@mindspring.com>
2024-02-01 00:08:27 -0500

    Split wifi config into multiple dt scripts

--- common_scripts/dt-wifi_ap.sh
+++ common_scripts/dt-wifi_ap.sh
@@ -25,23 +25,14 @@
 # version:     v1.0.0
 #####################################################################################
 
-# Fix file permissions (this was an issue on older images)
-sudo chmod 755 /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null 2>&1
-
 # Read settings from file
 SSID_LINE=$(sudo cat /etc/NetworkManager/system-connections/RobotAP.nmconnection | grep ssid=)
 PASS_LINE=$(sudo cat /etc/NetworkManager/system-connections/RobotAP.nmconnection | grep psk=)
-
-# Note: Need full path to iw b/c DeployTool doesn't run a shell, thus sbin not in path
-COUNTRY_LINE=$(/usr/sbin/iw reg get | grep country | head -1)
-
-
 CHANNEL_LINE=$(sudo cat /etc/NetworkManager/system-connections/RobotAP.nmconnection | grep channel=)
 BAND_LINE=$(sudo cat /etc/NetworkManager/system-connections/RobotAP.nmconnection | grep band=)
 SSID=$(echo "$SSID_LINE" | sed -z 's/ssid=//g')
 PASS=$(echo "$PASS_LINE" | sed -z 's/psk=//g')
-COUNTRY=$(echo "$COUNTRY_LINE" | sed -z 's/country //g')
-COUNTRY="${COUNTRY%:*}"
+
 CHANNEL=$(echo "$CHANNEL_LINE" | sed -z 's/channel=//g')
 BAND=$(echo "$BAND_LINE" | sed -z 's/band=//g')
 
@@ -49,14 +40,13 @@ BAND=$(echo "$BAND_LINE" | sed -z 's/band=//g')
 if [ $# -eq 0 ]; then
     echo "$SSID"
     echo "$PASS"
-    echo "$COUNTRY"
     echo "$CHANNEL"
     echo "$BAND"
     exit 0
 else
     if [ $# -ne 5 ]; then
-        echo "Either call with zero or five arguments!"
-        echo "$0 [NEW_SSID NEW_PASSWORD COUNTRY_CODE CHANNEL BAND]"
+        echo "Either call with zero or four arguments!"
+        echo "$0 [NEW_SSID NEW_PASSWORD CHANNEL BAND]"
         exit 1
     fi
 fi
--- common_scripts/dt-wifi_ap_replace.py
+++ common_scripts/dt-wifi_ap_replace.py
@@ -22,12 +22,4 @@ with fileinput.FileInput("/etc/NetworkManager/system-connections/RobotAP.nmconne
             print("channel=" + sys.argv[4])
         else:
             print(line, end='')
-
-# Country code not set via network manager
-# Use iw reg set to apply it now
-os.system("iw reg set {}".format(sys.argv[3]))
-
-# Write config file to apply it on boot
-with open("/etc/modprobe.d/cfg80211_regdomain.conf", "w") as file:
-    file.write("options cfg80211 ieee80211_regdom={}".format(sys.argv[3]))
     
