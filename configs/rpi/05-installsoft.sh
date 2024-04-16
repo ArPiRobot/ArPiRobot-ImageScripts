9e8a52d729cbd16beac4c411fec59103df69cafc
Marcus Behel <marcus.behel@mindspring.com>
2024-04-15 20:55:43 -0400

    Add ffmpeg to rpi image

--- configs/rpi/05-installsoft.sh
+++ configs/rpi/05-installsoft.sh
@@ -40,4 +40,5 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install git \
     libcamera0.2 \
     gstreamer1.0-libcamera \
     rpicam-apps-lite \
-    libopencv-*406
+    libopencv-*406 \
+    ffmpeg
