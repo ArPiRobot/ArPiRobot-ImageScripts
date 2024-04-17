5a8d16621105bf840fac38905624391abce6e400
Marcus Behel <marcus.behel@mindspring.com>
2024-04-16 22:20:18 -0400

    Sysroot package fixes

--- sysroot_setup_aarch64.sh
+++ sysroot_setup_aarch64.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# Enable other components of main repo
+sed -r -i 's/^deb(.*)$/deb\1 contrib/g' /etc/apt/sources.list
+sed -r -i 's/^deb(.*)$/deb\1 non-free/g' /etc/apt/sources.list
 apt-get update -y
+
 apt-get install -y \
     libserial-dev \
     libgstreamer1.0-dev \
