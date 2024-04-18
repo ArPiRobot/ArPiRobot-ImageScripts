13e14dcc515adf4a675a6e0dc9a9d66d59293c8d
Marcus Behel <marcus.behel@mindspring.com>
2024-04-18 19:54:07 -0400

    Fix aarch64 sysroot script

--- sysroot_setup_aarch64.sh
+++ sysroot_setup_aarch64.sh
@@ -14,18 +14,18 @@ apt-get install -y \
     libserialport-dev
 
 # pigpio and lgpio are not available in debian repos
-sudo apt-get install -y wget swig python3-dev python3-setuptools
+apt-get install -y wget swig python3-dev python3-setuptools
 wget https://github.com/joan2937/lg/archive/refs/tags/v0.2.2.tar.gz
 tar --extract --gzip -f v0.2.2.tar.gz
 cd lg-0.2.2
 make
-sudo make install
+make install
 cd ..
 rm -rf lg-0.2.2
 wget https://github.com/joan2937/pigpio/archive/refs/tags/v79.tar.gz
 tar --extract --gzip -f v79.tar.gz
 cd pigpio-79
 make
-sudo make install
+make install
 cd ..
 rm -rf pigpio-79
