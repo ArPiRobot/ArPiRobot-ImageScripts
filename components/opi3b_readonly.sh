2288ef9c27a3dd9e5624510f6a10a06fbd5b0b2d
Marcus Behel <marcus.behel@mindspring.com>
2024-04-21 23:54:47 -0400

    Fix incorrect command in opi image generation

--- components/opi3b_readonly.sh
+++ components/opi3b_readonly.sh
@@ -30,7 +30,7 @@ EOF
 chmod +x /usr/local/bin/dt-rw.sh
 
 # Remove services that we don't need that hold files open for writes
-DEBIAN_FRONTEND=noninteractive apt-get -y vnstat containerd.io
+DEBIAN_FRONTEND=noninteractive apt-get -y remove vnstat containerd.io
 
 # packagekitd holds file open for write after apt-get is used
 # not really sure if there are scenarios where we'd want to start this
